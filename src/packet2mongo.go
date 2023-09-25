d6ece3080a01133190711d0946e8e5b9364f1e8f
B00TK1D <stearns.josiah@gmail.com>
2023-09-25 00:17:35 -0400

    Added additional fields, supports nearly all tcp/udp data

--- src/packet2mongo.go
+++ src/packet2mongo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -24,31 +26,76 @@ const (
 	defaultSnapLen = 262144
 )
 
+type TCPFlag struct {
+	NS  bool
+	CWR bool
+	ECE bool
+	URG bool
+	ACK bool
+	PSH bool
+	RST bool
+	SYN bool
+	FIN bool
+}
+
 // Packet struct
 type Packet struct {
-	Src    string
-	Dst    string
-	TTL    uint8
-	Length uint16
-	Iface  string
+	Iface       string
+	Timestamp   int64
+	SrcMac      string
+	DstMac      string
+	IpVer       uint8
+	SrcIp       string
+	DstIp       string
+	TTL         uint8
+	Protocol    string
+	SrcPort     uint64
+	DstPort     uint64
+	TCPSeq      uint32
+	TCPAck      uint32
+	TCPFlag     TCPFlag
+	TCPWindow   uint16
+	TCPUrgent   uint16
+	TCPOptions  []layers.TCPOption
+	PayloadLen  uint16
+	Payload     []byte
+	CaptureTags []string
 }
 
 func main() {
 
 	// Read in cmd line args
 	if len(os.Args) != 3 {
-		log.Fatal("Usage: packet2mongo <interface> <mongodb_uri>")
+		log.Fatal("Usage: packet2mongo <interface> <mongodb_uri> [capture_tags]")
 	}
 	iface := os.Args[1]
 	mongoURI := os.Args[2]
 
+	// Get capture tags
+	var captureTags []string
+	if len(os.Args) > 3 {
+		captureTags = os.Args[3:]
+	}
+
+	// Get mongo port number from URI
+	mongoPort, err := strconv.ParseUint(mongoURI[len(mongoURI)-5:], 10, 16)
+	if err != nil {
+		mongoPort = 27017
+	}
+
 	// Open pcap handle
-	handle, err := pcap.OpenLive("en0", defaultSnapLen, true,
+	handle, err := pcap.OpenLive(iface, defaultSnapLen, true,
 		pcap.BlockForever)
 	if err != nil {
 		panic(err)
 	}
 	defer handle.Close()
+
+	// Ignore traffic on the mongodb port
+	err = handle.SetBPFFilter(fmt.Sprintf("not port %d", mongoPort))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Connect to mongodb
 	client, err := mongo.Connect(nil, options.Client().ApplyURI(mongoURI))
@@ -63,30 +110,88 @@ func main() {
 	packets := gopacket.NewPacketSource(
 		handle, handle.LinkType()).Packets()
 	for pkt := range packets {
-		// Add the packet to the database
+
+		packet := Packet{
+			Iface:       iface,
+			CaptureTags: captureTags,
+		}
+
+		// Get link layer
+		linkLayer := pkt.LinkLayer()
+		if linkLayer == nil {
+			continue
+		}
+		// Get network layer
 		netLayer := pkt.NetworkLayer()
 		if netLayer == nil {
 			continue
 		}
-		dst := netLayer.NetworkFlow().Dst().String()
-		src := netLayer.NetworkFlow().Src().String()
-		// Get packet ttl
+
+		// Get IP Layer
 		ipLayer := pkt.Layer(layers.LayerTypeIPv4)
-		if ipLayer == nil {
+		if ipLayer != nil {
+			packet.IpVer = 4
+			packet.TTL = ipLayer.(*layers.IPv4).TTL
+			packet.Protocol = ipLayer.(*layers.IPv4).Protocol.String()
+		} else {
+			// Try IPv6
+			ipLayer = pkt.Layer(layers.LayerTypeIPv6)
+			if ipLayer != nil {
+				continue
+			}
+			packet.IpVer = 6
+			packet.TTL = ipLayer.(*layers.IPv6).HopLimit
+			packet.Protocol = ipLayer.(*layers.IPv6).NextHeader.String()
+		}
+		// Get transport layer
+		transportLayer := pkt.TransportLayer()
+		if transportLayer == nil {
 			continue
 		}
-		ttl := ipLayer.(*layers.IPv4).TTL
-		// Get packet length
-		length := ipLayer.(*layers.IPv4).Length
-		// Insert packet into database
-		packet := Packet{
-			Src:    src,
-			Dst:    dst,
-			TTL:    ttl,
-			Length: length,
-			Iface:  iface,
+
+		packet.Timestamp = pkt.Metadata().Timestamp.UnixNano()
+		packet.SrcMac = linkLayer.LinkFlow().Src().String()
+		packet.DstMac = linkLayer.LinkFlow().Dst().String()
+		packet.DstIp = netLayer.NetworkFlow().Dst().String()
+		packet.SrcIp = netLayer.NetworkFlow().Src().String()
+		packet.SrcPort, err = strconv.ParseUint(transportLayer.TransportFlow().Src().String(), 10, 16)
+		if err != nil {
+			log.Fatal(err)
 		}
-		_, err := collection.InsertOne(nil, packet)
+		dstPort, err := strconv.ParseUint(transportLayer.TransportFlow().Dst().String(), 10, 16)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if transportLayer.LayerType() == layers.LayerTypeTCP {
+			tcpLayer := transportLayer.(*layers.TCP)
+			packet.TCPSeq = tcpLayer.Seq
+			packet.TCPAck = tcpLayer.Ack
+			packet.TCPFlag.NS = tcpLayer.NS
+			packet.TCPFlag.CWR = tcpLayer.CWR
+			packet.TCPFlag.ECE = tcpLayer.ECE
+			packet.TCPFlag.URG = tcpLayer.URG
+			packet.TCPFlag.ACK = tcpLayer.ACK
+			packet.TCPFlag.PSH = tcpLayer.PSH
+			packet.TCPFlag.RST = tcpLayer.RST
+			packet.TCPFlag.SYN = tcpLayer.SYN
+			packet.TCPFlag.FIN = tcpLayer.FIN
+
+			packet.TCPWindow = tcpLayer.Window
+			packet.TCPUrgent = tcpLayer.Urgent
+			packet.TCPOptions = tcpLayer.Options
+
+			packet.PayloadLen = uint16(len(tcpLayer.Payload))
+			packet.Payload = tcpLayer.Payload
+		} else if transportLayer.LayerType() == layers.LayerTypeUDP {
+			udpLayer := transportLayer.(*layers.UDP)
+			packet.DstPort = uint64(dstPort)
+			packet.PayloadLen = uint16(len(udpLayer.Payload))
+			packet.Payload = udpLayer.Payload
+		} else {
+			continue
+		}
+
+		_, err = collection.InsertOne(nil, packet)
 		if err != nil {
 			log.Fatal(err)
 		}
